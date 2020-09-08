401a768ba353a5f90ecf66f8cc94417db990d593
李志敏 <lizm@mingtech.cn>
2020-09-08 15:12:17 +0800

    add os

--- src/main/java/com/luffy/jdk/command/Test.java
+++ src/main/java/com/luffy/jdk/command/Test.java
@@ -1,10 +1,7 @@
 package com.luffy.jdk.command;
 
 import com.alibaba.fastjson.JSON;
-import com.luffy.jdk.command.util.ProcessUtil;
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.IOException;
 
 /**
  * Created by Luffy on 2020/9/7
@@ -14,9 +11,9 @@ public class Test {
 
     private static final OSScriptHandler HANDLER = OSScriptClient.instance();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String[] command = HANDLER.showIpAddr("eth0");
-        log.info("命令:{},执行结果:{}", JSON.toJSONString(command), ProcessUtil.getInputStreamAsString(ProcessUtil.exec(command).getInputStream()));
+        log.info("需要执行的命令及参数:{}", JSON.toJSONString(command));
 
     }
 }
