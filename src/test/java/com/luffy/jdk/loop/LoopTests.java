2463e17976d53cdcb65e6ae0438285c3ca3825be
李志敏 <lizm@mingtech.cn>
2020-09-27 10:08:51 +0800

    add date util

--- src/test/java/com/luffy/jdk/loop/LoopTests.java
+++ src/test/java/com/luffy/jdk/loop/LoopTests.java
@@ -1,7 +1,14 @@
 package com.luffy.jdk.loop;
 
+import com.luffy.jdk.util.DateUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -31,5 +38,40 @@ public class LoopTests {
             }
             log.info("i:{}" ,i);
         }
+    }
+
+
+    private static final ScheduledExecutorService EXECUTOR = Executors.newSingleThreadScheduledExecutor();
+
+
+    @Test
+    public void executor() throws InterruptedException {
+        log.info("Start a scheduled task");
+        EXECUTOR.scheduleWithFixedDelay(new TimerTask() {
+            @Override
+            public void run() {
+                log.info("你好啊,现在是: {} ",DateUtil.formatLocalDateTimeToString(DateUtil.getCurrentLocalDateTime(), DateUtil.DATE_FMT_13));
+            }
+        }, 0L,1L, TimeUnit.SECONDS);
+
+        TimeUnit.DAYS.sleep(20);
+    }
+
+    @Test
+    public void testExec() throws InterruptedException {
+        while (true){
+            log.info("距离凌晨还有: {} 分钟", getInterval());
+            TimeUnit.MINUTES.sleep(1L);
+        }
+    }
+
+    private static long getInterval(){
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_YEAR, 1);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return (cal.getTimeInMillis() - System.currentTimeMillis()) / 1000 / 60;
     }
 }
