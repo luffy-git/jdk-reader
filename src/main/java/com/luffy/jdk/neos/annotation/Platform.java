3bd10b83788f189d51af91e643a732f87c2b11cd
李志敏 <lizm@mingtech.cn>
2020-10-13 14:30:03 +0800

    新增字符串解析

--- src/main/java/com/luffy/jdk/neos/annotation/Platform.java
+++ src/main/java/com/luffy/jdk/neos/annotation/Platform.java
@@ -8,11 +8,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Created by Luffy on 2020/9/9
+ *  设备平台属性
+ * @author Luffy [lizm@mingtech.cn]
+ * @since 2020-10-13 14:29:43
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Platform {
 
+    /** 标记机械类型 */
     Machine machine();
 }
